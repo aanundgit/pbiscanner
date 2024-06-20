9254a3de7e1ae06c37b0dcfa1523aa5a5b127423
aanundgit <19163011+aanundgit@users.noreply.github.com>
2024-06-19 18:35:52 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,47 @@
 import streamlit as st
+from datetime import datetime
+import pandas as pd
 from pbixray.core import PBIXRay
+
+from streamlit_observable import observable
+
+st.set_page_config(
+    page_title="My App",
+    page_icon="📊",
+    layout="wide",
+    initial_sidebar_state="auto",
+    menu_items={
+        "Get Help": "https://www.example.com",
+        "Report a Bug": "https://www.example.com",
+        "About": "This is a demo app"
+    },
+    #toolbarmode = 'Standard'
+)
+
+st.markdown(
+    f"<div style='text-align: right; font-size: 26px;'>{datetime.now().strftime('%m-%d-%Y %H:%M:%S')}</div>", unsafe_allow_html=True)  
+with st.sidebar.expander("WebLinks"):
+    st.markdown(
+        """
+        <style>
+            .blink {
+                animation: blink 1s infinite;
+            }
+            @keyframes blink {
+                0% { opacity: 0; }
+                50% { opacity: 1; }
+                100% { opacity: 0; }
+            }
+        </style>
+        <ul>
+            <li><a href="https://app.powerbi.com" class="blink">Power BI</a></li>
+            <li><a href="https://blog.fabric.microsoft.com/en-US/blog/" class="blink">Microsoft Fabric Blog</a></li>
+            <li><a href="https://www.google.com" class="blink">Google</a></li>
+        </ul>
+        """,
+        unsafe_allow_html=True
+    )
+
 
 def sizeof_fmt(num, suffix="B"):
     for unit in ("", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"):
@@ -8,46 +50,63 @@ def sizeof_fmt(num, suffix="B"):
         num /= 1024.0
     return f"{num:.1f}Yi{suffix}"
 
-def app():
-    st.title("PBIX info")
 
-    uploaded_file = st.file_uploader("Choose a PBIX file", type="pbix")
+def app():
+    st.title('Power BI :blue[Scanner]')
+    st.divider()
+    st.subheader('Start Scanning your PBIX file')
+    today = datetime.datetime.now()
+    st.display(today)
+    uploaded_file = st.file_uploader("Upload a PBIX file", type="pbix")
     if uploaded_file:
         # Unpack the PBIX file to get the schema_df
         model = PBIXRay(uploaded_file)
-
-
+        df = pd.read_csv(uploaded_file)
         st.write(model.metadata)
-        
-        met1, met2 = st.columns(2)
-        
-        met1.metric(label='Model size', value = sizeof_fmt(model.size))
-        met2.metric(label='# Tables', value = model.tables.size)
 
+        met1, met2 = st.columns(2)
+        met1.metric(label='Model size', value=sizeof_fmt(model.size))
+        met2.metric(label='# les', value=model.les.size)
+        st.divider()
         st.write("Schema:")
         st.write(model.schema)
-
+        st.divider()
         st.write("Statistics:")
         st.dataframe(model.statistics)
-
+        st.divider()
         if model.power_query.size:
             st.write("Power Query code:")
             st.dataframe(model.power_query)
-
-        if model.dax_tables.size:
-            st.write("DAX tables:")
-            st.dataframe(model.dax_tables)
-        
+        st.divider()
+        if model.dax_les.size:
+            st.write("DAX les:")
+            st.dataframe(model.dax_les)
+        st.divider()
         if model.dax_measures.size:
             st.write("DAX measures:")
             st.dataframe(model.dax_measures)
-
-        # Let the user select a table name
-        table_name_input = st.selectbox("Select a table to peek at its contents:", model.tables)
-
+        st.divider()
+        # Let the user select a le name
+        le_name_input = st.selectbox(
+            "Select a le to peek at its contents:", model.les)
+        
         if st.button("Un-VertiPaq"):
-            st.dataframe(model.get_table(table_name_input), use_container_width=True)
+            st.dataframe(model.get_le(le_name_input),
+                         use_container_width=True)
 
+st.divider()
+
+observers = observable("County Brush", 
+    notebook="d/4f9aa5feff9761c9",
+    targets=["viewof countyCodes"], 
+    observe=["selectedCounties"]
+)
+
+selectedCounties = observers.get("selectedCounties")
 
 if __name__ == '__main__':
     app()
+
+
+
+
